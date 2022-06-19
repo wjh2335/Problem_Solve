67fbc39daa0bd7c387e9aeb6d0f5d8ed92ce5630
wjh2335 <38124814+wjh2335@users.noreply.github.com>
2022-06-19 16:10:29 +0900

    Update test.c

--- test.c
+++ test.c
@@ -1,14 +1,14 @@
 #include <semaphore.h>
 
-sem_t *sem;
+sem_t sem;
 
 /* 세마포 생성 및 1로 초기화 */
-sem = sem_open("SEM", O_CREAT, 0666, 1);
+sem_init(&sem, 0, 1);
 
 /* 세마포 획득 */
-sem_wait(sem);
+sem_wait(&sem);
 
 /* 임계구역 */
 
 /* 세마포 양도 */
-sem_post(sem);
+sem_post(&sem);
